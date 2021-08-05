8765dd48de1336bba38370ce24c1001046c98287
PengPengQ <1747447034@qq.com>
2021-08-05 21:40:28 +0800

    Update Ajax

--- demo/Form+AJAX/js/PengPengQ.js
+++ demo/Form+AJAX/js/PengPengQ.js
@@ -1,5 +1,16 @@
+/* 自定义Ajax的基本使用 */
+/* PengPengQ({
+ *   method: '请求类型',
+ *   url: '请求地址',
+ *   data: { 请求参数对象 },
+ *   success: function(res){ // 成功的回调函数
+ *     console.log(res)      // 打印数据
+ *   }
+ * })
+ */
+
 // 自己封装AJAX函数
-// 处理数据，得到参数
+// 处理数据，得到参数数据
 function resolveData(data) {
   var arr = [];
   for (k in data){
@@ -12,6 +23,7 @@ function resolveData(data) {
 function PengPengQ(option){
   var xhr = new XMLHttpRequest();
 
+  /* 拼接查询字符串 */
   var qs = resolveData(option.data);
 
   // xhr.open('GET', '');
@@ -26,7 +38,8 @@ function PengPengQ(option){
 
   xhr.onreadystatechange = function(){
     if (xhr.readyState == 4 && xhr.status == 200){
-      var respon = JSON.parse(this.responseText);
+      /* 将接收的JSON数据格式化为字符串 */
+      var respon = JSON.parse(xhr.responseText);
       option.success(respon);
     }
   }
